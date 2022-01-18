a05a0daef3d857ebb51340eff577fc5c4498d93f
HYEOK_E <jhjeong00@gmail.com>
2022-01-18 18:36:43 +0900

    chore: convert payment function modify

--- src/utils/hooks/paymentConvert.ts
+++ src/utils/hooks/paymentConvert.ts
@@ -8,13 +8,23 @@ export const paymentConvert = ({
   const convertPayAmount = payAmount / 10000;
   const convertPayType = () => {
     switch (payType) {
-      case 'MO':
-        return '월급';
-      case 'WE':
-        return '주급';
+      case 'HO':
+        return '시급';
       case 'DA':
         return '일급';
+      case 'WE':
+        return '주급';
+      case 'MO':
+        return '월급';
+      case 'YE':
+        return '연봉';
+      case 'IR':
+        return '무관';
     }
   };
-  return `${convertPayType()} ${convertPayAmount}만원`;
+  if (payType == 'IR') {
+    return convertPayType();
+  } else {
+    return `${convertPayType()} ${convertPayAmount}만원`;
+  }
 };
