26409813f5f9299976d9f04b8d66f9259e5d85a2
leeyun1533 <leeyun1533@naver.com>
2022-02-18 20:43:14 +0900

    fix: add safearea padding option to layout

--- src/styles/layout.ts
+++ src/styles/layout.ts
@@ -2,7 +2,5 @@ import styled from 'styled-components';
 
 export const LayoutContainer = styled.div`
   padding: 0 20px;
-  /* padding-top: 40px; */
-  position: relative;
-  top: 40px;
+  padding-top: calc(40px + safe-area-inset-top);
 `;
