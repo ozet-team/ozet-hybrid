9867414a6929e595f6fc6c7d8305f9e344aab789
leeyun1533 <leeyun1533@naver.com>
2022-02-18 20:10:22 +0900

    fix: layout test

--- src/styles/layout.ts
+++ src/styles/layout.ts
@@ -2,5 +2,7 @@ import styled from 'styled-components';
 
 export const LayoutContainer = styled.div`
   padding: 0 20px;
-  padding-top: 40px;
+  /* padding-top: 40px; */
+  position: relative;
+  top: 40px;
 `;
