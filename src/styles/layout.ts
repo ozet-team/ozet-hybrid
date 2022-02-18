1bb69cd2ec23124ca86293849c37eab514d1bd51
leeyun1533 <leeyun1533@naver.com>
2022-02-18 20:50:13 +0900

    fix: add env function to safe area

--- src/styles/layout.ts
+++ src/styles/layout.ts
@@ -2,5 +2,5 @@ import styled from 'styled-components';
 
 export const LayoutContainer = styled.div`
   padding: 0 20px;
-  padding-top: calc(40px + safe-area-inset-top);
+  padding-top: calc(40px + env(safe-area-inset-top));
 `;
