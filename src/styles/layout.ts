f949a1a84808b9ff88b27d1c21f5d14c46308c56
leeyun1533 <leeyun1533@naver.com>
2022-02-21 19:04:21 +0900

    fix: fix list layout safe-area

--- src/styles/layout.ts
+++ src/styles/layout.ts
@@ -2,14 +2,17 @@ import styled from 'styled-components';
 
 export const LayoutContainer = styled.div<{ isList?: boolean }>`
   position: relative;
-  top: ${(props) => (props.isList ? '102px;' : '44px;')}
+  top: ${(props) =>
+    props.isList
+      ? 'calc(102px + env(safe-area-inset-top));'
+      : 'calc(44px + env(safe-area-inset-top));'}
   padding: 0 20px;
   height: 100%;
   overflow: auto;
 `;
 
 export const ListLayoutContainer = styled.div`
-  height: 100%;
   overflow-y: hidden;
   padding-top: 20px;
+  height: calc(100% - 20px);
 `;
